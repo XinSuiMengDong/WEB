a269d85cba7f56705a0ea15a8ecb6b7edb2a98c0
xsmd <3514862572@qq.com>
2022-08-09 13:55:28 +0800

    A test

---
+++ new_script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://www.bilibili.com
+// @version      0.1
+// @description  Just a test.
+// @author       xsmd
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Just a test.')
+})();
