cae21035212b47f2068b1c21226a0b2b3d51c5cd
xsmd <3514862572@qq.com>
2022-08-09 14:51:55 +0800

    Add license

--- new_script.js
+++ new_script.js
@@ -7,6 +7,7 @@
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 (function() {
