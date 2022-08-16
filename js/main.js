542cb87fd9ac5eb9e7ebbdfeff89442aca72c7ee
xsmd <3514862572@qq.com>
2022-08-16 12:10:54 +0800

    add a sidebar.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ function uptotop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function openleftsb() {
+    document.getElementById("leftsb").style.width = "100px";
+}
